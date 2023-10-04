871ebe244d7099f7a3e8d84262f69834b5de426f
Arpit Shukla <126109611+Astron-1@users.noreply.github.com>
2023-10-04 23:54:56 +0530

    Add files via upload

---
+++ allApi.py
@@ -0,0 +1,112 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import io
+import numpy as np
+from rembg import remove
+from PIL import Image
+from webcolors import rgb_to_name, CSS3_HEX_TO_NAMES, hex_to_rgb
+from scipy.spatial import KDTree
+import os
+import requests
+
+app = FastAPI()
+
+class ImageRequest(BaseModel):
+    image_url: str
+
+class RGBRequest(BaseModel):
+    rgb: list[int]
+
+@app.post('/process_image')
+async def process_image(data: ImageRequest):
+    try:
+        # Fetch the image from the URL
+        response = requests.get(data.image_url)
+        response.raise_for_status()
+        input_img = Image.open(io.BytesIO(response.content))
+
+        # Remove background from the image
+        output_img = remove(input_img).convert("RGBA")
+
+        # Convert the image to a numpy array
+        img_array = np.array(output_img)
+
+        # Find the non-zero rows and columns
+        rows, cols = np.where(img_array[:, :, 3] != 0)
+        top_row, bottom_row = rows.min(), rows.max()
+        left_col, right_col = cols.min(), cols.max()
+
+        # Crop the image
+        cropped_img = output_img.crop((left_col, top_row, right_col, bottom_row))
+
+        # Get colors of cropped image
+        colors = cropped_img.getcolors(maxcolors=2**24)
+        newcolors = list(colors)
+
+        # Find the second most common color and remove it from the list of colors
+        def colorextraction():
+            first_most_common_color = max(newcolors, key=lambda x: x[0])
+            for i in newcolors:
+                if i == first_most_common_color:
+                    newcolors.remove(i)
+            fmc = first_most_common_color[1]
+            cfmc = first_most_common_color[0]
+            if fmc[0] != 0 and fmc[1] != 0 and fmc[2] != 0:
+                return fmc, cfmc
+            else:
+                return colorextraction()
+
+        rc = colorextraction()
+        fmc = rc[0]
+        cfmc = rc[1]
+
+        # Find the second most common color and remove it from the list of colors
+        second_most_common_color = max(newcolors, key=lambda x: x[0])
+        for i in newcolors:
+            if i == second_most_common_color:
+                newcolors.remove(i)
+        smc = second_most_common_color[1]
+        csmc = second_most_common_color[0]
+
+        # Return results as JSON
+        result = {
+            'second_most_common_color': smc,
+            'second_most_common_count': csmc,
+        }
+
+        return result
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post('/get_color_name')
+async def get_color_name(data: RGBRequest):
+    rgb_tuple = tuple(data.rgb)
+
+    try:
+        # first try to get the name of the color directly
+        color_name = rgb_to_name(rgb_tuple, spec='css3')
+    except ValueError:
+        # if the color is not found, find the closest matching color
+        css3_db = CSS3_HEX_TO_NAMES
+        names = []
+        rgb_values = []
+        for color_hex, color_name in css3_db.items():
+            names.append(color_name)
+            rgb_values.append(hex_to_rgb(color_hex))
+        kdt_db = KDTree(rgb_values)
+        distance, index = kdt_db.query(rgb_tuple)
+        color_name = names[index]
+
+    # Return results as JSON
+    result = {
+        'color_name': color_name,
+    }
+    
+    return result
+
+if __name__ == '__main__':
+    from uvicorn import run
+    hostval = '0.0.0.0'
+    port1 = int(os.environ.get('PORT_APP1', 5000))
+    run(app, host=hostval, port=port1)
